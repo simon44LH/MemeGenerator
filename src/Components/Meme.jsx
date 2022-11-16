c295c42427413d90ca9eb97fe42e5da2392d72ed
simon44LH <103035547+simon44LH@users.noreply.github.com>
2022-11-16 14:37:29 +0100

    memes werden jetzt dynamisch generiert

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,21 +1,43 @@
 import React from "react";
+import memesData from "../Data/memesData";
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/9ehk.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage(e) {
+    e.preventDefault();
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
   return (
-    <div>
-      <form>
+    <div className="meme-main">
+      <form className="form" onSubmit={(e) => getMemeImage(e)}>
         <label>
-          <input className="input-fields" type="text" placeholder="Shut up" />
+          <input className="input-fields" type="text" placeholder="Top text" />
         </label>
         <label>
           <input
             className="input-fields"
             type="text"
-            placeholder="and take my money"
+            placeholder="Bottom text"
           />
         </label>
-        <input className="Button" type="submit" value="Get a new meme image" />
+        <button className="Button" type="submit" value="">
+          Get a new meme image
+        </button>
       </form>
+      <img src={meme.randomImage} className="memeImage" />
     </div>
   );
 }
